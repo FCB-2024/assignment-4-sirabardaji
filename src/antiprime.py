1b31648d9cf6b5caa658c0e286080dcee89d5b71
Sira <sira.bardaji01@estudiant.upf.edu>
2024-10-22 15:38:49 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,8 +1,7 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
-	x=int(input("Enter a positive integer number: "))
+def main(x) :
 	y=1
 	r=0
 	## YOU CODE SHOULD START HERE AST THE SAME
@@ -14,19 +13,19 @@ def main() :
 
 	l=x-1
 	s=0
-	while l<=1 and s<r:
-		a=1
+	while l>=1 and s<r:
+		c=1
 		s=0
-		while a<=l:
-			if l % a == 0:
+		while c<=l:
+			if l % c == 0:
 				s=s+1
-			a=a+1
+			c=c+1
 		l=l-1
 		
 	if s>=r:
-		return("anti-prime")
-	else:	
 		return("not anti-prime")
+	else:	
+		return("anti-prime")
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
@@ -37,10 +36,13 @@ def main() :
 	
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys
+	if len(sys.argv) > 1:
+		x= int (sys.argv [1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main(int(sys.argv[1])))
+		print(main(x))
